f64d4a6d69602744565b195a03b42e2fafeb2bb3
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-11 06:11:26 +0100

    FEAT aggiunte variabili bombe e tentativi

--- js/app.js
+++ js/app.js
@@ -6,7 +6,16 @@ let latoGriglia = 10;
 // Calcoliamo il numero totale di celle nella griglia
 let numeroCelle = latoGriglia * latoGriglia;
 
+// Definiamo il numero massimo di tentativi consentiti
+let maxTentativi = numeroCelle - 16;
+
+// Inizializziamo l'array delle bombe
 let bombe = [];
+
+// Inizializziamo la variabile dei tentativi
+let tentativi = 0;
+
+let punteggio = 0;
 
 // Selezioniamo il pulsante "startGame" dalla pagina HTML
 const startButton = document.querySelector("#startGame");
@@ -20,78 +29,105 @@ const restartButton = document.querySelector("#restartGame");
 // Nascondiamo il pulsante "restartGame" all'inizio
 restartButton.style.display = "none";
 
+let caselleNonBombePremute = 0;
+
 // Aggiungiamo un evento al clic sul pulsante "startGame"
 startButton.addEventListener("click", function () {
-  // Rimuoviamo il pulsante "startGame" dalla pagina
-  document.querySelector("#startGame").remove();
+// Rimuoviamo il pulsante "startGame" dalla pagina
+document.querySelector("#startGame").remove();
 
-  for (let i = 0; i < 16; i++) {
-    const numero = Math.floor(Math.random() * numeroCelle) + 1;
-    bombe.push(numero);
-
-    console.log(bombe)
+// Generiamo l'array delle bombe
+function generaBombe() {
+bombe = [];
+for (let i = 0; i < 16; i++) {
+let numero = Math.floor(Math.random() * numeroCelle) + 1;
+  // Verifichiamo che il numero non sia già presente nell'array e che non si ripeta la posizione
+  while (bombe.includes(numero) || (i === 0 && numero === 1) || bombe.includes(numero - 1) || bombe.includes(numero + 1) || bombe.includes(numero + latoGriglia) || bombe.includes(numero - latoGriglia)) {
+    numero = Math.floor(Math.random() * numeroCelle) + 1;
   }
 
+  bombe.push(numero);
+  console.log(bombe);
+}
+}
 
-  // Mostriamo il pulsante "restartGame"
-  restartButton.style.display = "block";
+generaBombe();
 
-  // Creiamo un ciclo per generare tutte le celle della griglia
-  for (let i = 0; i < numeroCelle; i++) {
-    let num = i + 1;
+// Mostriamo il pulsante "restartGame"
+restartButton.style.display = "block";
 
-    // Creiamo una stringa HTML per ogni cella
-    let divString =
-      "<div class='cella' id='cella-" +
-      num +
-      "' style='width: calc(100% / " +
-      latoGriglia +
-      ")'>" +
-      num +
-      "</div>";
-
-    // Aggiungiamo la stringa HTML all'elemento della griglia
-    grigliaElement.innerHTML += divString;
-  }
-
-  // Selezioniamo tutti gli elementi con classe "cella" dalla pagina HTML
-  const cella = document.querySelectorAll(".cella");
-
-  // Iteriamo su ogni cella e associamo l'evento "click"
-  cella.forEach(function (cella) {
-    cella.addEventListener("click", function () {
-      cella.classList.add("bg-green");
-      // Logghiamo l'ID della cella cliccata
-      console.log(cella.id.split("-")[1]);
-      if (cella.id == bombe){
-        alert('Hai perso!')
-      }
-    });
-  });
-});
-
-
-// Aggiungiamo un evento al clic sul pulsante "restartGame"
 restartButton.addEventListener("click", function () {
-  grigliaElement.innerHTML = "";
-
-  for (let i = 0; i < numeroCelle; i++) {
-    let num = i + 1;
-    let divString =
-      "<div class='cella' id='cella-" +
-      num +
-      "' style='width: calc(100% / " +
-      latoGriglia +
-      ")'>" +
-      num +
-      "</div>";
-    grigliaElement.innerHTML += divString;
-  }
-
-  const cella = document.querySelectorAll(".cella");
-  cella.forEach(function (cella) {
-    cella.addEventListener("click", function () {
-      cella.classList.add("bg-green");
-    });
-  });
+// Reset del gioco
+generaBombe();
+tentativi = 0;
+punteggio = 0;
+caselleNonBombePremute = 0;
+grigliaElement.innerHTML = "";
 });
+
+// Creiamo un ciclo per generare tutte le celle della griglia
+for (let i = 0; i < numeroCelle; i++) {
+let num = i + 1;
+// Creiamo una stringa HTML per ogni cella
+let divString =
+"<div class='cella' id='cella-" +
+   num +
+   "' style='width: calc(100% / " +
+   latoGriglia +
+   ")'>" +
+num +
+"</div>";
+// Aggiungiamo la stringa HTML all'elemento della griglia
+grigliaElement.innerHTML += divString;
+}
+
+// Selezioniamo tutti gli elementi con classe "cella" dalla pagina HTML
+const celle = document.querySelectorAll(".cella");
+
+// Iteriamo su ogni cella e associamo l'evento "click"
+celle.forEach(function (cella) {
+  cella.addEventListener("click", function () {
+  if (cella.classList.contains("bg-green") || cella.classList.contains("bg-red")) {
+  // Se la cella è già stata cliccata, esci dalla funzione
+  return;
+  }
+  // Se la cella non è una bomba, coloriamola di verde e incrementiamo il punteggio
+  if (!bombe.includes(parseInt(cella.id.split("-")[1]))) {
+  cella.classList.add("bg-green");
+  caselleNonBombePremute++;
+  // Se sono state premute tutte le caselle non bombe, l'utente ha vinto
+  if (caselleNonBombePremute === numeroCelle - 16) {
+  alert("Hai vinto! Punteggio: " + caselleNonBombePremute);
+  }
+  } else {
+  // Se la cella è una bomba, coloriamola di rosso e terminiamo il gioco
+  cella.classList.add("bg-red");
+  alert("Hai perso! Punteggio: " + caselleNonBombePremute);
+    // Reset del gioco dopo 2 secondi
+    setTimeout(function() {
+      bombe = [];
+      tentativi = 0;
+      punteggio = 0;
+      caselleNonBombePremute = 0;
+      grigliaElement.innerHTML = "";
+      startButton.click();
+    }, 2000);
+  }
+  tentativi++;
+  
+  // Se il numero di tentativi raggiunge il massimo consentito, terminiamo il gioco
+  if (tentativi === maxTentativi) {
+    alert("Hai perso! Punteggio: " + caselleNonBombePremute);
+  
+    // Reset del gioco dopo 2 secondi
+    setTimeout(function() {
+      bombe = [];
+      tentativi = 0;
+      punteggio = 0;
+      caselleNonBombePremute = 0;
+      grigliaElement.innerHTML = "";
+      startButton.click();
+    }, 2000);
+  }
+});
+})});  
